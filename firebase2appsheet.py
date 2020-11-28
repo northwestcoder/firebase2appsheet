11e180dbd0043fb187be75f8b95c5bb8192daaf0
Ty Alevizos <northwestcoder@gmail.com>
2020-11-28 13:47:51 -0800

    adding content support

--- firebase2appsheet.py
+++ firebase2appsheet.py
@@ -22,6 +22,7 @@ events = db.collection('events')
 places = db.collection('places')
 things = db.collection('things')
 settings = db.collection('settings')
+contents = db.collection('contents')
 
 
 # settings and event polling
@@ -57,7 +58,7 @@ def heartbeat():
 			print("status is OFF, heartbeat mode paused")	
 			heartbeatstatus = db.collection(u'settings').document(u'heartbeat')
 			status = heartbeatstatus.get().to_dict()["value"]
-		time.sleep(10)
+		time.sleep(60)
 
 
 @app.route('/settings', methods=['GET'])
@@ -298,6 +299,56 @@ def deletething(id):
 		return f"An Error Occurred: {e}"
 
 
+@app.route('/contents', methods=['POST'])
+def createcontent():
+
+  try:
+    id = request.json['id']
+    contents.document(id).set(request.json)
+    return jsonify({"success": True}), 200
+  except Exception as e:
+    return f"An Error Occurred: {e}"
+
+@app.route('/contents', methods=['GET'])
+def readcontent():
+
+  try:
+    all_contents = [doc.to_dict() for doc in contents.stream()]
+    jsonresult = jsonify({"contents" : all_contents}), 200
+    return jsonresult
+  except Exception as e:
+    return f"An Error Occurred: {e}"
+
+@app.route('/contents/<id>', methods=['GET'])
+def readonecontent(id):
+
+  try:
+    content = contents.document(id).get()
+    return jsonify(content.to_dict()), 200
+  except Exception as e:
+    return f"An Error Occurred: {e}"
+
+
+@app.route('/contents/<id>', methods=['PUT'])
+def updatecontent(id):
+
+  try:
+    id = request.json['id']
+    contents.document(id).update(request.json)
+    return jsonify({"success": True}), 200
+  except Exception as e:
+    return f"An Error Occurred: {e}"
+
+
+@app.route('/contents/<id>', methods=['DELETE'])
+def deletecontent(id):
+
+  try:
+    contents.document(id).delete()
+    return jsonify({"success": True}), 200
+  except Exception as e:
+    return f"An Error Occurred: {e}"
+    
 port = int(os.environ.get('PORT', 8080))
 if __name__ == '__main__':
 	heartbeat_process = Process(target=heartbeat)
