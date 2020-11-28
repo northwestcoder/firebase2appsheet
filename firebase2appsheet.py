16c169e486ad342b05a444f40a4ee65c61d6261b
Ty Alevizos <northwestcoder@gmail.com>
2020-11-28 07:48:55 -0800

    added places to oas

--- firebase2appsheet.py
+++ firebase2appsheet.py
@@ -15,7 +15,7 @@ default_app = initialize_app(cred)
 db = firestore.client()
 persons = db.collection('persons')
 events = db.collection('events')
-
+places = db.collection('places')
 
 @app.route('/oaspec', methods=['GET'])
 def getoas():
@@ -78,8 +78,6 @@ def deleteperson(id):
 		return f"An Error Occured: {e}"
 
 
-
-
 @app.route('/events', methods=['POST'])
 def createevent():
 
@@ -132,6 +130,59 @@ def deleteevent(id):
 	except Exception as e:
 		return f"An Error Occured: {e}"
 
+
+
+@app.route('/places', methods=['POST'])
+def createplace():
+
+  try:
+    id = request.json['id']
+    places.document(id).set(request.json)
+    return jsonify({"success": True}), 200
+  except Exception as e:
+    return f"An Error Occured: {e}"
+
+@app.route('/places', methods=['GET'])
+def readplace():
+
+  try:
+    all_places = [doc.to_dict() for doc in places.stream()]
+    jsonresult = jsonify({"places" : all_places}), 200
+#   appsheetresult["places"] = jsonresult
+    return jsonresult
+#   return jsonify("places" : all_places), 200
+  except Exception as e:
+    return f"An Error Occured: {e}"
+
+@app.route('/places/<id>', methods=['GET'])
+def readoneplace(id):
+
+  try:
+    todo = places.document(id).get()
+    return jsonify(todo.to_dict()), 200
+  except Exception as e:
+    return f"An Error Occured: {e}"
+
+
+@app.route('/places/<id>', methods=['POST', 'PUT'])
+def updateplace(id):
+
+  try:
+    id = request.json['id']
+    places.document(id).update(request.json)
+    return jsonify({"success": True}), 200
+  except Exception as e:
+    return f"An Error Occured: {e}"
+
+
+@app.route('/places/<id>', methods=['DELETE'])
+def deleteplace(id):
+
+  try:
+    places.document(id).delete()
+    return jsonify({"success": True}), 200
+  except Exception as e:
+    return f"An Error Occured: {e}"
 
 
 
